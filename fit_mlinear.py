da958f9901a3f875fd20c7307f3cdf91de24e109
guilongcheng <guilongcheng@ihep.ac.cn>
2017-10-20 14:42:50 +0800

    mac: add m>n case

--- fit_mlinear.py
+++ fit_mlinear.py
@@ -12,7 +12,7 @@ def multifit(x,y,m):
     """
     n = len(x)
     if m > n :
-        print("error: m >n")
+        print("warning: m >n")
 
     S = np.zeros((m+1,m+1))
     T = np.zeros(m+1)
@@ -41,6 +41,7 @@ def main():
     m=5
     a = multifit(x,y,m)
     a = a[::-1]
+    print("a is ",a)
 
     plt.plot(x,y,'o',label='data')
     plt.plot(x,np.polyval(a,x),'-',label="fit")
@@ -48,13 +49,14 @@ def main():
     plt.ylabel("y")
     plt.legend()
     plt.show()
-
-    x = np.linspace(-5,2,50)
+#==============================================
+    x = np.linspace(-50,50,50)
     y = np.exp(x)
 
     m=5
     a = multifit(x,y,m)
     a = a[::-1]
+    print("a is ",a)
 
     plt.plot(x,y,'o',label='data')
     plt.plot(x,np.polyval(a,x),'-',label="fit")
@@ -62,7 +64,33 @@ def main():
     plt.ylabel("y")
     plt.legend()
     plt.show()
+#==============================================
+    x = np.linspace(1,2,2)
+    def linear(x,a):
+        y = np.zeros(len(x))
+        for i in range(len(a)):
+            y = y + x**i*a[i]
+        return y
+    y = linear(x,[4,3,2])
 
+
+    a = multifit(x,y,2)
+    a = a[::-1]
+    print("a is :",a)
+
+    b = multifit(x,y,1)
+    b = b[::-1]
+    print("b is :",b)
+
+    xplot=np.arange(-1,3,0.1)
+    plt.plot(x,y,'o',label='data')
+    plt.plot(xplot,np.polyval(a,xplot),'-',label="fit 3")
+    plt.plot(xplot,np.polyval(b,xplot),'-',label="fit 2")
+    plt.plot(xplot,linear(xplot,[4,3,2]),'-',label="real")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
     main()
